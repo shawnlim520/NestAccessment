03db24efb82d5eddd0d2f354a1fe376bb16be5eb
Shawn <lx.lim@cck.com.my>
2022-10-16 11:30:57 +0800

    added import to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmployeeModule } from './employee/employee.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [EmployeeModule],
-
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), EmployeeModule, PrismaModule],
 })
 export class AppModule {}
