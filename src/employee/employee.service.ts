e4709665979aba3e6f3be6c8f1718c4dc3db68f1
Shawn <lx.lim@cck.com.my>
2022-10-16 11:19:25 +0800

    Work on Employee service

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -6,5 +6,66 @@ import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class EmployeeService{
   constructor(private prisma: PrismaService){}
+  getEmployees() {
+    return this.prisma.employee.findMany()
+}
 
+
+ async createEmployee(dto: CreateEmployeeDto) {
+    try {
+        const user = await this.prisma.employee.create({
+            data: {
+                username:dto.username,
+                fullname:dto.fullname,
+                salary:dto.salary,
+            }
+        })
+        return user; 
+    } catch (error) {
+        if (error instanceof PrismaClientKnownRequestError) {
+            if (error.code === 'P2002') {
+                throw new ForbiddenException('Credentials taken');
+            }
+        }
+    }
+
+}
+
+
+async getEmployeeById(userId:number) {
+    const user = await this.prisma.employee.findUnique({
+        where: {
+            id: userId
+        }
+    })
+
+    return user;
+}
+
+
+async editEmployeeById(userId:number, dto: EditEmployeeDto) {
+    const user = await this.prisma.employee.update({
+        where: {
+            id: userId
+        },
+        data: {
+            username:dto.username,
+            fullname:dto.fullname,
+            salary:dto.salary,
+        }
+    })
+
+    return user;
+}
+
+
+async deleteEmployeeById(userId:number) {
+    const user = await this.prisma.employee.delete({
+        where: {
+            id:userId
+        }
+    })
+
+    return user;
+}
 }
