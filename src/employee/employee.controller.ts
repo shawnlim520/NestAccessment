e246a5168d85b673909b1b030aac1b339be4ddb4
Shawn <lx.lim@cck.com.my>
2022-10-16 11:15:10 +0800

    Work on Employee controller

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -5,5 +5,44 @@ import { EmployeeService } from "./employee.service";
 @Controller('employees')
 export class EmployeeController{
     constructor(private EmployeeService: EmployeeService){}
+    @Get()
+    getemployees(
+    ) {
+        return this.EmployeeService.getEmployees()
+    }
+
+    @Get(':id')
+    getEmployeeById(
+        @Param('id',ParseIntPipe) employeeId:number,
+    ) {
+        return this.EmployeeService.getEmployeeById(employeeId)
+    }
+
+    @Post()
+    createEmployee(
+        @Body() dto: CreateEmployeeDto,
+    ) {
+        return this.EmployeeService.createEmployee(
+            dto
+        )
+    }
+
+    @Patch(':id')
+    editEmployeeById(
+        @Param('id',ParseIntPipe) employeeId:number,
+        @Body() dto: EditEmployeeDto,
+    ) {
+        return this.EmployeeService.editEmployeeById(
+            employeeId,
+            dto
+        )
+    }
+
+    @Delete(':id')
+    deleteEmployeeById(
+        @Param('id',ParseIntPipe) employeeId:number,
+    ) {
+        return this.EmployeeService.deleteEmployeeById(employeeId)
+    }
 
 }
